2274a80e054555ddc878ed78262f7a6f8a061bc5
Nathan <nathankirley@gmail.com>
2020-06-24 14:27:43 +0100

    customer class written

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,45 @@
+import vehicles.Vehicle;
+
+import java.util.ArrayList;
+
 public class Customer {
+
+    private String name;
+    private int money;
+    private ArrayList<Vehicle> vehicles;
+
+    public Customer(String name, int money, ArrayList<Vehicle> vehicles){
+        this.name = name;
+        this.money = money;
+        this.vehicles = vehicles;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getMoney(){
+        return this.money;
+    }
+
+    public ArrayList<Vehicle> getVehicles(){
+        return this.vehicles;
+    }
+
+    public void addToVehicles(Vehicle vehicle){
+        this.vehicles.add(vehicle);
+    }
+
+    public int getVehicleCount(){
+        return this.vehicles.size();
+    }
+
+    public void spendMoney(int money){
+        this.money -= money;
+    }
+
+    public void buy(Vehicle vehicle){
+        addToVehicles(vehicle);
+        spendMoney(vehicle.getPrice());
+    }
 }
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,2 +1,48 @@
+import org.junit.Before;
+import org.junit.Test;
+import parts.Engine;
+import parts.FuelTank;
+import vehicles.Bicycle;
+import vehicles.HybridCar;
+import vehicles.Vehicle;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
+    private Engine engine;
+    private FuelTank fuelTank;
+    private HybridCar hybridCar;
+    private Bicycle bicycle;
+    private Customer customer;
+    private ArrayList<Vehicle> vehicles;
+
+    @Before
+    public void before() {
+        engine = new Engine(140);
+        fuelTank = new FuelTank("Unleaded", 200);
+        hybridCar = new HybridCar("Nissan", "Micra", 35000, engine, fuelTank);
+        bicycle = new Bicycle("Raleigh", "Motus", 2000);
+        vehicles = new ArrayList<Vehicle>();
+        Collections.addAll(vehicles, hybridCar, bicycle);
+        customer = new Customer("Nathan", 100000, vehicles);
+    }
+    
+    @Test
+    public void hasName() {
+        assertEquals("Nathan", customer.getName());
+    }
+
+    @Test
+    public void hasMoney() {
+        assertEquals(100000, customer.getMoney());
+    }
+
+    @Test
+    public void hasVehicles() {
+        assertEquals(100000, customer.getMoney());
+    }
+
 }
