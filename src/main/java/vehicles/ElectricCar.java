f9bebb5f1201f01b25a1c9ae411f6d97d436fc28
Nathan <nathankirley@gmail.com>
2020-06-24 14:07:55 +0100

    electriccar tests written

--- src/main/java/vehicles/ElectricCar.java
+++ src/main/java/vehicles/ElectricCar.java
@@ -1,4 +1,16 @@
 package vehicles;
 
-public class ElectricCar {
+public class ElectricCar extends MotorVehicle {
+    private int chargeLevel;
+
+    public ElectricCar(String manufacturer, String model, int price, Engine engine) {
+        super(manufacturer, model, price, engine);
+        this.chargeLevel = 100;
+    }
+
+    public int getChargeLevel() {
+        return this.chargeLevel;
+    }
+
+    public void useCharge(int amount)
 }
--- src/test/java/ElectricCarTest.java
+++ src/test/java/ElectricCarTest.java
@@ -2,58 +2,59 @@ import org.junit.Before;
 import org.junit.Test;
 import parts.Engine;
 import vehicles.Car;
+import vehicles.ElectricCar;
 
 import static org.junit.Assert.assertEquals;
 
 public class ElectricCarTest {
-//    private Engine engine;
-//    private Car car;
-//
-//    @Before
-//    public void before() {
-//        engine = new Engine(140);
-//        car = new Car("Ford", "Mondeo", 24000, engine);
-//    }
-//
-//    @Test
-//    public void hasManufacturer() {
-//        assertEquals("Ford", car.getManufacturer());
-//    }
-//
-//    @Test
-//    public void hasModel() {
-//        assertEquals("Mondeo", car.getModel());
-//    }
-//
-//    @Test
-//    public void hasPrice() {
-//        assertEquals(24000, car.getPrice());
-//    }
-//
-//    @Test
-//    public void canAddDamage() {
-//        car.addDamage(4000);
-//        assertEquals(4000, car.getDamage());
-//        assertEquals(20000, car.getPrice());
-//    }
-//
-//    @Test
-//    public void canRepairDamage() {
-//        car.addDamage(4000);
-//        car.repairDamage();
-//        assertEquals(0, car.getDamage());
-//        assertEquals(24000, car.getPrice());
-//    }
-//
-//    @Test
-//    public void damageCannotExceedPrice() {
-//        car.addDamage(25000);
-//        assertEquals(0, car.getDamage());
-//        assertEquals(24000, car.getPrice());
-//    }
-//
-//    @Test
-//    public void hasEngine() {
-//        assertEquals(engine, car.getEngine());
-//    }
+    private Engine engine;
+    private ElectricCar electricCar;
+
+    @Before
+    public void before() {
+        engine = new Engine(140);
+        electricCar = new ElectricCar("Honda", "Jazz", 30000, engine);
+    }
+
+    @Test
+    public void hasManufacturer() {
+        assertEquals("Honda", electricCar.getManufacturer());
+    }
+
+    @Test
+    public void hasModel() {
+        assertEquals("Jazz", electricCar.getModel());
+    }
+
+    @Test
+    public void hasPrice() {
+        assertEquals(30000, electricCar.getPrice());
+    }
+
+    @Test
+    public void canAddDamage() {
+        electricCar.addDamage(4000);
+        assertEquals(4000, electricCar.getDamage());
+        assertEquals(26000, electricCar.getPrice());
+    }
+
+    @Test
+    public void canRepairDamage() {
+        electricCar.addDamage(4000);
+        electricCar.repairDamage();
+        assertEquals(0, electricCar.getDamage());
+        assertEquals(30000, electricCar.getPrice());
+    }
+
+    @Test
+    public void damageCannotExceedPrice() {
+        electricCar.addDamage(30000);
+        assertEquals(0, electricCar.getDamage());
+        assertEquals(30000, electricCar.getPrice());
+    }
+
+    @Test
+    public void hasEngine() {
+        assertEquals(engine, electricCar.getEngine());
+    }
 }
