87cc285ec85ce2c44f25b2aaff60ed0860a81737
Nathan <nathankirley@gmail.com>
2020-06-24 13:58:18 +0100

    car tests pass

--- src/main/java/vehicles/Car.java
+++ src/main/java/vehicles/Car.java
@@ -1,4 +1,10 @@
 package vehicles;
 
-public class Car {
+import parts.Engine;
+
+public class Car extends MotorVehicle {
+
+    public Car(String manufacturer, String model, int price, Engine engine){
+        super(manufacturer, model, price, engine);
+    }
 }
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,2 +1,58 @@
+import org.junit.Before;
+import org.junit.Test;
+import parts.Engine;
+import vehicles.Car;
+
+import static org.junit.Assert.assertEquals;
+
 public class CarTest {
+
+    private Engine engine;
+    private Car car;
+
+    @Before
+    public void before() {
+        engine = new Engine(140);
+        car = new Car("Ford", "Mondeo", 24000, engine);
+    }
+
+    @Test
+    public void hasManufacturer() {
+        assertEquals("Ford", car.getManufacturer());
+    }
+
+    @Test
+    public void hasModel() {
+        assertEquals("Mondeo", car.getModel());
+    }
+
+    @Test
+    public void hasPrice() {
+        assertEquals(24000, car.getPrice());
+    }
+
+    @Test
+    public void canAddDamage() {
+        car.addDamage(4000);
+        assertEquals(4000, car.getDamage());
+        assertEquals(20000, car.getPrice());
+    }
+
+    @Test
+    public void canRepairDamage() {
+        car.addDamage(4000);
+        car.repairDamage();
+        assertEquals(0, car.getDamage());
+        assertEquals(24000, car.getPrice());
+    }
+
+    @Test
+    public void damageCannotExceedPrice() {
+        car.addDamage(25000);
+        assertEquals(0, car.getDamage());
+        assertEquals(24000, car.getPrice());
+    }
+
+
+
 }
